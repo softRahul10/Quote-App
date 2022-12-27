7699ad54d120b3d08bb2d49a662bb3d62b9787a0
softRahul10 <softrahul10@gmail.com>
2022-12-28 04:12:56 +0530

    fetch method add

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Js file')
+// Get Quote from API
+async function getQuote() {
+    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try{
+        const response = await fetch(apiURL);
+        const data = await response.json();
+    } catch(error) {
+        console.log('No,Quote' ,error);
+    }
+}
+
+// On page Load
+getQuote();
